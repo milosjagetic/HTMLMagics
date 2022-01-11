f55eca05728dfc698c6e988e694f9e3483bf4cb3
Miloš Jagetić <milos.jagetic@gmail.com>
2022-01-11 22:52:17 +0100

    initial

---
+++ Tests/HTMLMagicsTests/HTMLMagicsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import HTMLMagics
+
+final class HTMLMagicsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(HTMLMagics().text, "Hello, World!")
+    }
+}
